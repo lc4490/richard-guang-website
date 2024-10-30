6864223d4027d296f4f08bce25c475a320bae5d3
Leo <lc4490@nyu.edu>
2024-10-29 20:18:19 -0400

    no extra empty lisitng

--- app/listings/page.js
+++ app/listings/page.js
@@ -111,111 +111,116 @@ export default function Home() {
               display="flex"
               justifyContent="center"
             >
-              <Card
-                sx={{
-                  width: "100%",
-                  maxWidth: 400,
-                  //   borderRadius: "16px",
-                  //   boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)", // Subtle shadow for elevation
-                  //   transition: "transform 0.3s ease", // Hover effect
-                  //   "&:hover": {
-                  //     transform: "scale(1.05)", // Hover scale effect
-                  //   },
-                }}
-              >
-                {listing.image && (
-                  <CardMedia>
-                    <Image
-                      src={listing.image}
-                      alt="listing image"
-                      width={400}
-                      height={250}
-                      style={{
-                        // borderTopLeftRadius: "16px",
-                        // borderTopRightRadius: "16px",
-                        objectFit: "cover",
-                        width: 400,
-                        height: 250,
+              {listing.url && (
+                <Card
+                  sx={{
+                    width: "100%",
+                    maxWidth: 400,
+                    //   borderRadius: "16px",
+                    //   boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)", // Subtle shadow for elevation
+                    //   transition: "transform 0.3s ease", // Hover effect
+                    //   "&:hover": {
+                    //     transform: "scale(1.05)", // Hover scale effect
+                    //   },
+                  }}
+                >
+                  {listing.image && (
+                    <CardMedia>
+                      <Image
+                        src={listing.image}
+                        alt="listing image"
+                        width={400}
+                        height={250}
+                        style={{
+                          // borderTopLeftRadius: "16px",
+                          // borderTopRightRadius: "16px",
+                          objectFit: "cover",
+                          width: 400,
+                          height: 250,
+                        }}
+                      />
+                    </CardMedia>
+                  )}
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
+                    >
+                      {listing.address}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {listing.location} New York, New York,{" "}
+                      {listing.postal_code}
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        marginTop: 1,
+                        fontWeight: "bold",
+                        color: "green",
                       }}
-                    />
-                  </CardMedia>
-                )}
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant="h6"
-                    sx={{ fontWeight: "bold", fontSize: "1.2rem" }}
-                  >
-                    {listing.address}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    {listing.location} New York, New York, {listing.postal_code}
-                  </Typography>
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      marginTop: 1,
-                      fontWeight: "bold",
-                      color: "green",
-                    }}
-                  >
-                    {listing.price__value}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    {listing["listing-item__feat__value"] &&
-                      `${listing["listing-item__feat__value"]} ${
-                        listing["listing-item__feat__description"]
-                          ? listing["listing-item__feat__description"]
-                              .charAt(0)
-                              .toUpperCase() +
-                            listing["listing-item__feat__description"].slice(1)
-                          : ""
-                      }`}
-                    {listing["listing-item__feat__value 2"] &&
-                      ` | ${listing["listing-item__feat__value 2"]} ${
-                        listing["listing-item__feat__description 2"]
-                          ? listing["listing-item__feat__description 2"]
-                              .charAt(0)
-                              .toUpperCase() +
-                            listing["listing-item__feat__description 2"].slice(
-                              1
-                            )
-                          : ""
-                      }`}
-                    {listing["listing-item__feat__value 3"] &&
-                      ` | ${listing["listing-item__feat__value 3"]} ${
-                        listing["listing-item__feat__description 3"]
-                          ? listing["listing-item__feat__description 3"]
-                              .charAt(0)
-                              .toUpperCase() +
-                            listing["listing-item__feat__description 3"].slice(
-                              1
-                            )
-                          : ""
-                      }`}
-                  </Typography>
-                </CardContent>
-                <CardActions sx={{ justifyContent: "center", paddingY: 2 }}>
-                  <Button
-                    href={listing?.url || "#"}
-                    target="_blank"
-                    variant="contained"
-                    color="primary"
-                    sx={{ borderRadius: "50px" }}
-                  >
-                    {t("View Details")}
-                  </Button>
-                  <Button
-                    href={"/contact"}
-                    // target="_blank"
-                    variant="contained"
-                    color="primary"
-                    sx={{ borderRadius: "50px" }}
-                  >
-                    {t("Inquire")}
-                  </Button>
-                </CardActions>
-              </Card>
+                    >
+                      {listing.price__value}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {listing["listing-item__feat__value"] &&
+                        `${listing["listing-item__feat__value"]} ${
+                          listing["listing-item__feat__description"]
+                            ? listing["listing-item__feat__description"]
+                                .charAt(0)
+                                .toUpperCase() +
+                              listing["listing-item__feat__description"].slice(
+                                1
+                              )
+                            : ""
+                        }`}
+                      {listing["listing-item__feat__value 2"] &&
+                        ` | ${listing["listing-item__feat__value 2"]} ${
+                          listing["listing-item__feat__description 2"]
+                            ? listing["listing-item__feat__description 2"]
+                                .charAt(0)
+                                .toUpperCase() +
+                              listing[
+                                "listing-item__feat__description 2"
+                              ].slice(1)
+                            : ""
+                        }`}
+                      {listing["listing-item__feat__value 3"] &&
+                        ` | ${listing["listing-item__feat__value 3"]} ${
+                          listing["listing-item__feat__description 3"]
+                            ? listing["listing-item__feat__description 3"]
+                                .charAt(0)
+                                .toUpperCase() +
+                              listing[
+                                "listing-item__feat__description 3"
+                              ].slice(1)
+                            : ""
+                        }`}
+                    </Typography>
+                  </CardContent>
+                  <CardActions sx={{ justifyContent: "center", paddingY: 2 }}>
+                    <Button
+                      href={listing?.url || "#"}
+                      target="_blank"
+                      variant="contained"
+                      color="primary"
+                      sx={{ borderRadius: "50px" }}
+                    >
+                      {t("View Details")}
+                    </Button>
+                    <Button
+                      href={"/contact"}
+                      // target="_blank"
+                      variant="contained"
+                      color="primary"
+                      sx={{ borderRadius: "50px" }}
+                    >
+                      {t("Inquire")}
+                    </Button>
+                  </CardActions>
+                </Card>
+              )}
             </Grid>
           ))}
         </Grid>
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "img-de-v2.gtsstatic.net",
-      "mmsmedia.vht.com",
-      "static-ind-elliman-newyorkcity-production.gtsstatic.net",
-    ], // Add the domains where your images are hosted
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img-de-v2.gtsstatic.net",
+        pathname: "/reno/imagereader.aspx/**", // Allows all images from this path
+      },
+      {
+        protocol: "https",
+        hostname: "mmsmedia.vht.com",
+        pathname: "/Media/**", // Allows images from this specific path
+      },
+      {
+        protocol: "https",
+        hostname: "static-ind-elliman-newyorkcity-production.gtsstatic.net",
+        pathname: "/resources/**", // Allows images from this specific path
+      },
+    ],
   },
 };
 
